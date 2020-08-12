d1c8e9dda69268a4fe841396f7b8ec22fa19ad04
prabhuchira <prabhuchira@gmail.com>
2020-08-12 11:32:53 +0530

    added github support

--- script.js
+++ script.js
@@ -9,25 +9,84 @@
     
 // })
 
+const playEvent = (ev) =>{
+    var audio = new Audio(`./sounds/` + ev).play();
+}
+
 (()=>{
-    var array = ["c_major.wav","csharp_major.wav","d_major.wav","dsharp_major.wav","e_major.wav","f_major.wav","fsharp_major.wav","g_major.wav","gsharp_major.wav","a_major.wav","asharp_major.wav","b_major.wav"]
+    var array = [
+        {key:"c_major.wav",keyCode:"a"},
+        {key:"csharp_major.wav",keyCode:"w"},
+        {key:"d_major.wav",keyCode:"s"},
+        {
+            key:"dsharp_major.wav",
+            keyCode:"e"
+        },
+        {
+            key:"e_major.wav",
+            keyCode:"d"
+        },
+        {
+            key:"f_major.wav",
+            keyCode:"r"
+        },  
+        {
+            key:"fsharp_major.wav",
+            keyCode:"f"
+        },
+        {
+            key:"g_major.wav",
+            keyCode:"t"
+        },
+        {
+            key:"gsharp_major.wav",
+            keyCode:"g"
+        },
+        {
+            key:"a_major.wav",
+            keyCode:"y"
+        },
+        {
+            key:"asharp_major.wav",
+            keyCode:"h"
+        },
+        {
+            key:"b_major.wav",
+            keyCode:"u"
+        },
+
+       ]
     array.forEach(item=>{
-        console.log(item.replace("_major.wav",""))
-        console.log(item)
+        console.log(item.key.replace("_major.wav",""))
+        // console.log(item)
 
         
-        document.getElementById(item.replace("_major.wav","")).addEventListener("mousedown",async()=>{
-            console.log(item.replace("_major.wav",""))
-            // console.log('windows')
-            var audio = new Audio(`./sounds/` + item).play();
+        document.getElementById((item.key).replace("_major.wav","")).addEventListener("mousedown",async()=>{
+            console.log(item.key.replace("_major.wav",""))
+            playEvent(item.key)
             console.log(item,"this is the item")
             
             
         })
+
         
     
     })
     
+    
+    window.addEventListener("keypress",(e)=>{
+        
+       array.forEach(item=>{
+           
+        if(e.key == item.keyCode){
+          
+           playEvent(item.key)
+        }
 
+       })
+    }
+        
+        
+    )
 
 })()
